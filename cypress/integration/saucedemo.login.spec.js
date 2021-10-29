81cd1626cb410dc10dc75699fd546199a75f4571
Genadi Dharma <genadislawa@gmail.com>
2021-10-29 16:30:47 +0800

    starter code for each test case added

---
+++ cypress/integration/saucedemo.login.spec.js
@@ -0,0 +1,20 @@
+/*
+    Nama    : YOUR_NAME
+    NIM     : YOUR_NIM
+    Kelas   : TI-3D
+*/
+
+/// <reference types="cypress"/>
+
+describe('YOUR_TEST_CASE', () => {
+
+    beforeEach(() => {
+        // The step you'll always run every scenario, eg: Login
+        cy.visit(Cypress.config().baseUrl)
+    })
+
+    it('YOUR_SCENARIO', () => {
+
+    })
+
+})
