5d9c4907f1ba671f20c4c15ff85795854745bd75
khosyrobbin <khosybin@gmail.com>
2021-10-30 09:13:30 +0700

    Login Test - Khosy Robbin Hood

--- cypress/integration/saucedemo.login.spec.js
+++ cypress/integration/saucedemo.login.spec.js
@@ -1,20 +1,57 @@
 /*
-    Nama    : YOUR_NAME
-    NIM     : YOUR_NIM
+    Nama    : Khosy Robbin Hood
+    NIM     : 1941720067
     Kelas   : TI-3D
 */
 
 /// <reference types="cypress"/>
 
-describe('YOUR_TEST_CASE', () => {
+describe('Login Page - Login Test', () => {
 
     beforeEach(() => {
         // The step you'll always run every scenario, eg: Login
-        cy.visit(Cypress.config().baseUrl)
+        cy.visit('https://www.saucedemo.com/')
     })
 
-    it('YOUR_SCENARIO', () => {
-
+    // Test 1
+    it('Standard User', () => {
+        cy.get('#user-name').type('standard_user')
+        cy.get('#password').type('secret_sauce')
+        cy.get('#login-button').click()
     })
 
+    // Test 2
+    it('Standard User - Invalid Password', () => {
+        cy.get('#user-name').type('standard_user')
+        cy.get('#password').type('secret_sambal')
+        cy.get('#login-button').click()
+    });
+
+    // Test 3
+    it('Standard User - Invalid Username', () => {
+        cy.get('#user-name').type('User_biasa')
+        cy.get('#password').type('secret_sauce')
+        cy.get('#login-button').click()
+    });
+
+    // Test 4
+    it('Standard User - Invalid Username & Invalid Password', () => {
+        cy.get('#user-name').type('user123')
+        cy.get('#password').type('password123')
+        cy.get('#login-button').click()
+    });
+
+    // Test 5
+    it('Locked Out User', () => {
+        cy.get('#user-name').type('locked_out_user')
+        cy.get('#password').type('secret_sauce')
+        cy.get('#login-button').click()
+    });
+
+    // Test 6
+    it.only('Problem User', () => {
+        cy.get('#user-name').type('problem_user')
+        cy.get('#password').type('secret_sauce')
+        cy.get('#login-button').click()
+    });
 })
